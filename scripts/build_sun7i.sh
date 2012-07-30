51915bfd7730962437e6580404195bbec1ea1917
jshwang <jshwang@Exdroid7.(none)>
2012-07-30 15:49:41 +0800

    sun7i: add mali driver

--- scripts/build_sun7i.sh
+++ scripts/build_sun7i.sh
@@ -112,12 +112,12 @@ build_modules()
 	make -C modules/example LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} \
 		CONFIG_CHIP_ID=${CONFIG_CHIP_ID} install
 
-#	(
-#	export LANG=en_US.UTF-8
-#	unset LANGUAGE
-#	make -C modules/mali LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} \
-#		CONFIG_CHIP_ID=${CONFIG_CHIP_ID} install
-#	)
+	(
+	export LANG=en_US.UTF-8
+	unset LANGUAGE
+	make -C modules/mali LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LICHEE_KDIR=${LICHEE_KDIR} \
+		CONFIG_CHIP_ID=${CONFIG_CHIP_ID} install
+	)
 
 #	#build usi-bmc4329 sdio wifi module
 #	make -C modules/wifi/usi-bcm4329/v4.218.248.15/open-src/src/dhd/linux \
